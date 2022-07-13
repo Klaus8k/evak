f246a2f735363b154cb53be76c59605a6b59d266
Oleg Sokolov <klaus888@yandex.ru>
2022-07-13 15:45:15 +0300

    add style and html

--- evak_main/views.py
+++ evak_main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, context={'title': 'evak24-7.ru'}, template_name='evak_main/index.html')
